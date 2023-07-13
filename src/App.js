887608203942666dfe95ecf6af497f1c6942f591
Tanushri <tanubadi@gmail.com>
2023-07-13 22:37:32 +0530

    Routing redirect issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React,{useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const [newTask,setNewTask] = useState("");
+ const [todoList,setTodoList] = useState([]);
+ 
+ const handleTask = (event) => {
+      setNewTask(event.target.value)
+ }
+ const addTask = () => {
+ // setTodoList ([...todoList,newTask])
+  const tanuTask = [...todoList,newTask]
+  setTodoList(tanuTask)
+ }
 
+ const deleteTask = (taskName) => {
+    const newTodoList = todoList.filter((task) => {
+      if (task === taskName){
+      return false;
+      } else {
+        return true;
+      }
+    })
+    setTodoList(newTodoList)
+ }
+  return (
+<>
+ <div className="container">
+  <div className="todo">
+    <input className="text" placeholder="Enter the Names here" onChange={handleTask}/>
+    <button className="button" onClick={addTask}>Add Names</button>
+  </div>
+ </div>
+ <h3>Todo List</h3>
+ <div className="list">
+  {todoList.map((task) => {
+    return (
+    <>
+    <h1>{task}</h1>
+    <button className="remove" onClick={() => deleteTask(task)}> Delete</button>
+    </>
+    )
+  })}
+ </div>
+</>
+  )
+  }
 export default App;
